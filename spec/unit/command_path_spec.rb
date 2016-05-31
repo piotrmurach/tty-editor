b6c3c1ed2e0831d05907eebb30e5b20081cbb2bb
Piotr Murach <pmurach@gmail.com>
2016-05-31 23:21:33 +0100

    Ensure escaping is fully tested.

--- spec/unit/command_path_spec.rb
+++ spec/unit/command_path_spec.rb
@@ -2,19 +2,19 @@
 
 RSpec.describe TTY::Editor, '#command' do
   it 'escapes editor filename on unix' do
-    editor = TTY::Editor.new("/bin/ruby hello.rb")
+    editor = TTY::Editor.new("/usr/bin/hello world.rb")
     allow(TTY::Editor).to receive(:command).and_return('vim')
     allow(TTY::Platform).to receive(:unix?).and_return(true)
 
-    expect(editor.command_path).to eql("vim /bin/ruby\\ hello.rb")
+    expect(editor.command_path).to eql("vim /usr/bin/hello\\ world.rb")
   end
 
-  it "doesn't shell escape on windows" do
-    editor = TTY::Editor.new("/bin/ruby hello.rb")
+  it "escapes path separators on windows" do
+    editor = TTY::Editor.new('C:\User\hello world.rb')
     allow(TTY::Editor).to receive(:command).and_return('vim')
     allow(TTY::Platform).to receive(:unix?).and_return(false)
     allow(TTY::Platform).to receive(:windows?).and_return(true)
 
-    expect(editor.command_path).to eql("vim \\bin\\ruby hello.rb")
+    expect(editor.command_path).to eql("vim C:\\User\\hello world.rb")
   end
 end
